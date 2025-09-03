620219632abf6dfe6e0ab0c77a53cdf59ca5ed3e
Sarthak <sarthakss898@gmail.com>
2025-09-03 16:28:47 +0530

    API Routes

--- first-next-app/src/app/page.tsx
+++ first-next-app/src/app/page.tsx
@@ -1,15 +1,29 @@
-import Link from "next/link";
 
-export default async function Home() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-  const post = await res.json();
+'use client'
+import Link from "next/link";
+import { useState } from "react";
+
+
+export default function Home() {
+  const [result, setResult] = useState<any>(null);
+
+  const sendData = async () => {
+    const res = await fetch('/api/hello', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text: 'Hello from Next.js' }),
+    });
+
+    setResult(await res.json());
+    console.log(result);
+  };
 
   return (
-    <>
+    <div>
       <h1 className="font-bold">Welcome to my home page</h1>
-      <Link href="/about">Go to about page</Link>
-      {post.title}
-      {post.body}
-    </>
+      <Link href="/about">Go to about page</Link><br />
+      <button onClick={sendData}>Send POST</button><br />
+      {result && JSON.stringify(result["recieved"]["text"])}      
+    </div>
   );
 }
