2cef171ead55edd9c32a6a6708742be75fc77a0e
Sarthak <sarthakss898@gmail.com>
2025-09-03 16:14:21 +0530

    Data fetching

--- first-next-app/src/app/page.tsx
+++ first-next-app/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const post = await res.json();
+
   return (
     <>
       <h1 className="font-bold">Welcome to my home page</h1>
       <Link href="/about">Go to about page</Link>
+      {post.title}
+      {post.body}
     </>
   );
 }
