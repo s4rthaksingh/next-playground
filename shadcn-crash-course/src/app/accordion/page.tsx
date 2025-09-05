3b9baaecbb0df651e3753032760f04a96f3b235e
Sarthak <sarthakss898@gmail.com>
2025-09-06 01:20:15 +0530

    Customize accordion

--- shadcn-crash-course/src/app/accordion/page.tsx
+++ shadcn-crash-course/src/app/accordion/page.tsx
@@ -7,14 +7,17 @@ import {
 
 function accord() {
     return (
-        <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-            </AccordionItem>
-        </Accordion>
+        <div className="flex justify-center items-center h-full border-2">
+            <main className="w-[500px] p-4"><Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+            </main>
+        </div>
     )
 }
 
