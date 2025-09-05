8d1a92cfe0795c865be75e54b9361231e6cb430d
Sarthak <sarthakss898@gmail.com>
2025-09-06 01:12:59 +0530

    Install accordion

--- shadcn-crash-course/src/app/sarthak/page.tsx
+++ shadcn-crash-course/src/app/sarthak/page.tsx
@@ -5,8 +5,8 @@ function MyPage() {
   return (
     <div className="h-full flex justify-center items-center flex-col">
         <div>MyPage :D</div>
-        <button className='px-5 py-2 bg-white text-black rounded-md hover:bg-gray-300'>Click me!</button>
-        <Button>Shadcn button</Button>
+        <button className='px-5 py-2 bg-white text-black rounded-md hover:bg-gray-300 m-5'>Click me!</button>
+        <Button variant={'destructive'} size={'sm'}>Shadcn button</Button>
     </div>
   )
 }
