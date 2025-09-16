af4daa159af7d1e83cf647919a428c8a12b1f3c1
Sarthak <sarthakss898@gmail.com>
2025-09-16 12:20:00 +0530

    Initialize project

---
+++ shadcn-docs/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Is this a shadcn button</Button>
+    </div>
+  );
+}
