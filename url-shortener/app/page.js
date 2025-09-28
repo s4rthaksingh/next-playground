1a13fcabef25681936ed414ea18024db2a26f5c6
Sarthak <sarthakss898@gmail.com>
2025-09-28 22:51:48 +0530

    URL Input field

--- url-shortener/app/page.js
+++ url-shortener/app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
       <div className="text-center text-3xl flex flex-col gap-6">
         <p>The best URL shortener in the world</p>
         <p>We are the most straightforward URL shortener in the world</p>
+        <input type="text" className="bg-gray-200 focus:border border-gray-800 rounded-lg p-2 px-4 text-2xl w-1/2 self-center focus:outline-0" placeholder="Enter a link"/>
       </div>
     </section>
   </main>
