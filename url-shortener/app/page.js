f3fd8aedd6f415b4a0aa2163693568781e190f29
Sarthak <sarthakss898@gmail.com>
2025-09-28 22:36:23 +0530

    Adjust home page heading

--- url-shortener/app/page.js
+++ url-shortener/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
     <section className="flex justify-center items-center h-full">
-      <div className="">
+      <div className="text-center text-3xl flex flex-col gap-6">
         <p>The best URL shortener in the world</p>
         <p>We are the most straightforward URL shortener in the world</p>
       </div>
