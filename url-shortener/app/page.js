bd6434b0277767c3bc800ccf32f893d0476215e5
Sarthak <sarthakss898@gmail.com>
2025-09-28 23:01:54 +0530

    Shorten button UI

--- url-shortener/app/page.js
+++ url-shortener/app/page.js
@@ -1,13 +1,15 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-    <section className="flex justify-center items-center h-full">
+    <section className="flex flex-col justify-center items-center h-full gap-10">
       <div className="text-center text-3xl flex flex-col gap-6">
         <p>The best URL shortener in the world</p>
         <p>We are the most straightforward URL shortener in the world</p>
-        <input type="text" className="bg-gray-200 focus:border border-gray-800 rounded-lg p-2 px-4 text-2xl w-1/2 self-center focus:outline-0" placeholder="Enter a link"/>
+      </div>
+      <div className=" flex gap-10">
+      <input type="text" className="bg-gray-200 focus:border border-gray-800 rounded-lg p-2 px-4 text-2xl w-3/4 self-center focus:outline-0" placeholder="Enter a link"/>
+      <button className="bg-purple-500 rounded-lg px-6 py-2 text-white font-medium hover:bg-purple-600"> Shorten</button>
       </div>
     </section>
   </main>
